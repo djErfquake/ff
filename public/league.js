d7a6c21e015e0561128b12acb7ae429792573bbf
clevally <clevally@roto.com>
2018-09-21 14:20:57 -0400

    starting collecing info on players

--- public/league.js
+++ public/league.js
@@ -25,7 +25,10 @@ $.getJSON('https://games.espn.com/ffl/api/v2/teams?leagueId=' + ESPN_LEAGUE_ID, 
     let seasonPercentage = ((currentGameWeek + 1) / totalGameWeeks) * 100;
     $('.season-progress').css('width', seasonPercentage.toString() + "%");
 
-    //
+    // add matchup info
+    let totalMatchups = currentGameWeek * teams.length;
+    let matchupsAdded = 0;
+
     for (let matchupPeriodIndex = 0; matchupPeriodIndex < currentGameWeek; matchupPeriodIndex++) {
       for (let teamIndex = 0; teamIndex < teams.length; teamIndex++) {
         let teamId = teams[teamIndex].teamId;
@@ -34,21 +37,55 @@ $.getJSON('https://games.espn.com/ffl/api/v2/teams?leagueId=' + ESPN_LEAGUE_ID, 
         '&teamId=' + teamId,
         function(boxscoreData) {
           if (teams[teamIndex].boxscores == undefined) {teams[teamIndex].boxscores = [];}
-          teams[teamIndex].boxscores.push(boxscoreData.boxscore);
+          teams[teamIndex].boxscores[matchupPeriodIndex] = boxscoreData.boxscore;
 
-          /*
-          let boxscorePlayers = boxscoreData.boxscore.teams[0].slots;
-          for (let playerIndex = 0; playerIndex < boxscorePlayers.length; playerIndex++) {
-              teams.players[boxscorePlayers[playerIndex].playerId] = boxscorePlayers[playerIndex];
-          }
-          */
-
+          matchupsAdded++;
+          //console.log(matchupsAdded + "/" + totalMatchups);
+          if (matchupsAdded == totalMatchups) { analyzeData(); }
         });
       }
     }
 
 
 
-    console.log("teams", teams);
   });
 });
+
+
+let analyzeData = function() {
+
+  let player1 = { points: 0 };
+  let player2 = { points: 0 };
+  let player3 = { points: 0 };
+
+  for (let teamIndex = 0; teamIndex < teams.length; teamIndex++) {
+    for (let matchupPeriodIndex = 0; matchupPeriodIndex < currentGameWeek; matchupPeriodIndex++) {
+
+      let boxscore = teams[teamIndex].boxscores[matchupPeriodIndex];
+      for (let matchupTeamIndex = 0; matchupTeamIndex < 2; matchupTeamIndex++) {
+
+        let boxscorePlayers = boxscore.teams[matchupTeamIndex].slots;
+        for (let playerIndex = 0; playerIndex < boxscorePlayers.length; playerIndex++) {
+
+          if (boxscorePlayers[playerIndex].currentPeriodProjectedStats != undefined) {
+            let playerScore = boxscorePlayers[playerIndex].currentPeriodProjectedStats.appliedStatTotal;
+            //console.log("Checking ", boxscorePlayers[playerIndex].player.firstName + " " + boxscorePlayers[playerIndex].player.lastName)
+
+            if (playerScore > player1.points) { player1.points = playerScore; player1.player = boxscorePlayers[playerIndex]; }
+            else if (playerScore > player2.points) { player2.points = playerScore; player2.player = boxscorePlayers[playerIndex]; }
+            else if (playerScore > player3.points) { player3.points = playerScore; player3.player = boxscorePlayers[playerIndex]; }
+          }
+        }
+      }
+    }
+  }
+
+  console.log("player 1", player1);
+  console.log("player 2", player2);
+  console.log("player 3", player3);
+
+
+
+
+  console.log("teams", teams);
+};
